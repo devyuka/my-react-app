eb3d826d5f38101ab3b228098d099990f1c3995e
Yuka <yuka.0531.a@gmail.com>
2020-04-21 19:46:18 -0700

    Complete the about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,33 @@
 import React from "react";
+import styles from "./about.module.css";
+import Title from "./../UI/Title/Title";
+import Button from "./../UI/Button/Button";
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <section>
+      <Title>About us</Title>
+      <p className={styles.outline}>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+        ligula eget dolor. Aenean massa. Lorem ipsum dolor sit amet,
+        consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
+        massa.
+        <br />
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+        ligula eget dolor. Aenean massa. Lorem ipsum dolor sit amet,
+        consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
+        massa.
+        <br />
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+        ligula eget dolor. Aenean massa. Lorem ipsum dolor sit amet,
+        consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
+        massa.
+      </p>
+
+      <p className={styles.message}>Please feel free to send us a message.</p>
+      <Button pathName="/contact">Contact</Button>
+    </section>
+  );
 };
 
 export default about;
